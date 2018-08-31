0beed91491f30ed423a359f0ddcf31c9b3ade10b
thiennt <nguyentungthien@gmail.com>
2018-08-31 17:25:07 +0700

    Fix rspec 1

--- app/models/street.rb
+++ app/models/street.rb
@@ -3,6 +3,6 @@ class Street < ApplicationRecord
   has_many :communes, through: :commune_streets
 
   validates_presence_of :title
-  validates_numericality_of :from, :allow_nil => true
-  validates_numericality_of :to, :allow_nil => true, :greater_than => :from, :if => Proc.new {|street| street.from.present? }
+  validates_numericality_of :from, :to, :allow_nil => true
+  validates_numericality_of :to, :greater_than => :from, :if => Proc.new {|street| street.from.present? }
 end
